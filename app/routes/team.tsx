9b3f5cbc3e3629c6f7811485097e421bf8dc18bf
Diana Lin <lindiana1206@gmail.com>
2025-08-23 17:08:07 -0400

    merge team page

--- app/routes/team.tsx
+++ app/routes/team.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-import Navbar from "~/navbar/navbar";
+import Navbar from "../navbar";
 import teamData from 'app/members.json'
+import Footer from "~/footer/footer";
 
 // JSON-like data for all committees
 export default function Team() {
@@ -33,58 +34,61 @@ export default function Team() {
 
 
     return (
-        <div className="flex min-h-screen bg-transparent text-white mx-20 my-30">
-            <Navbar/>
+        <div>
+            <div className="flex min-h-screen bg-transparent text-white mx-20 my-30">
+                <Navbar />
 
-            {/* Sidebar */}
-            <aside className="w-1/4 h-screen sticky top-0 p-4 flex pt-10 flex-col">
-                <ul className="space-y-6 text-center">
-                    {teamData.map((c) => (
-                        <li key={c.committee}>
-                            <a
-                                href={`#${c.committee}`}
-                                className={`block transition ${
-                                    active === c.committee
+                {/* Sidebar */}
+                <aside className="w-1/4 h-screen sticky top-0 p-4 flex pt-10 flex-col">
+                    <ul className="space-y-6 text-center">
+                        {teamData.map((c) => (
+                            <li key={c.committee}>
+                                <a
+                                    href={`#${c.committee}`}
+                                    className={`block transition ${active === c.committee
                                         ? "text-blue-400 font-bold"
                                         : "text-gray-400 hover:text-white"
-                                }`}
-                            >
-                                {c.committee}
-                            </a>
-                        </li>
-                    ))}
-                </ul>
-            </aside>
-
-
-            {/* Main content */}
-            <main className="flex-1 p-1 space-y-24">
-                <h1 className="text-4xl font-bold text-center mb-12">Meet The Team</h1>
-                {teamData.map((c) => (
-                    <section
-                        key={c.committee}
-                        id={c.committee}
-                        className="scroll-mt-20"
-                    >
-                        <div className="grid grid-cols-2 md:grid-cols-3 gap-6 mb-40">
-                            {c.members.map((member, i) => (
-                                <div
-                                    key={i}
-                                    className="flex flex-col items-center text-center space-y-2"
+                                        }`}
                                 >
-                                    <img
-                                        src={member.image}
-                                        alt={member.name}
-                                        className="w-45 h-50 object-cover rounded-xl border-2 border-blue-400"
-                                    />
-                                    <p className="font-medium">{member.name}</p>
-                                    <p className="text-sm text-gray-400">{c.committee}</p>
-                                </div>
-                            ))}
-                        </div>
-                    </section>
-                ))}
-            </main>
+                                    {c.committee}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </aside>
+
+
+                {/* Main content */}
+                <main className="flex-1 p-1 space-y-24">
+                    <h1 className="text-4xl font-bold text-center mb-12">Meet The Team</h1>
+                    {teamData.map((c) => (
+                        <section
+                            key={c.committee}
+                            id={c.committee}
+                            className="scroll-mt-20"
+                        >
+                            <div className="grid grid-cols-2 md:grid-cols-3 gap-6 mb-40">
+                                {c.members.map((member, i) => (
+                                    <div
+                                        key={i}
+                                        className="flex flex-col items-center text-center space-y-2"
+                                    >
+                                        <img
+                                            src={member.image}
+                                            alt={member.name}
+                                            className="w-45 h-50 object-cover rounded-xl border-2 border-blue-400"
+                                        />
+                                        <p className="font-medium">{member.name}</p>
+                                        <p className="text-sm text-gray-400">{c.committee}</p>
+                                    </div>
+                                ))}
+                            </div>
+                        </section>
+                    ))}
+                </main>
+
+            </div>
+            <Footer />
         </div>
     );
 }
