957e680d055c0b9a163e5d459671e96f2444c9d0
uravtanna <uravtanna@gmail.com>
2025-08-23 15:34:28 -0400

    fixed nav extending hero page bug

--- app/navbar/navbar.tsx
+++ app/navbar/navbar.tsx
@@ -4,13 +4,13 @@ export default function Navbar() {
     ];
 
     return (
-        <nav className="w-full top-0 left-0 z-50 bg-transparent">
+        <nav className="w-full absolute top-0 left-0 z-50 bg-transparent">
             <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
                 {/* Logo */}
                 <img src="app/navbar/hoohacks-owl-logo.svg" alt="Logo" />
 
                 {/* Navigation Links + Button (grouped on right) */}
-                <div className="flex items-center space-x-8">
+                <div className="flex items-center space-x-12">
                     <ul className="flex space-x-8">
                         {navItems.map((item) => (
                             <li key={item.name}>
