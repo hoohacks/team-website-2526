8da2833d9594eb11e695ccc8a08f77cfe93c0e3a
uravtanna <uravtanna@gmail.com>
2025-08-23 15:30:19 -0400

    added routing to team page:

--- app/navbar/navbar.tsx
+++ app/navbar/navbar.tsx
@@ -1,16 +1,16 @@
 export default function Navbar() {
     const navItems = [
-        { name: "Meet The Team", href: "#team" },
+        { name: "Meet The Team", href: "/team" },
     ];
 
     return (
-        <nav className="w-full absolute top-0 left-0 z-50 bg-transparent">
+        <nav className="w-full top-0 left-0 z-50 bg-transparent">
             <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
                 {/* Logo */}
                 <img src="app/navbar/hoohacks-owl-logo.svg" alt="Logo" />
 
                 {/* Navigation Links + Button (grouped on right) */}
-                <div className="flex items-center space-x-12">
+                <div className="flex items-center space-x-8">
                     <ul className="flex space-x-8">
                         {navItems.map((item) => (
                             <li key={item.name}>
