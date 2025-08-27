3a6b498312c80462fc0d6b9bc86063dc59f5818e
Diana Lin <lindiana1206@gmail.com>
2025-08-27 19:55:36 -0400

    add favicon

--- app/root.tsx
+++ app/root.tsx
@@ -21,6 +21,11 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Chakra+Petch:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swa",
   },
+  {
+    rel: "icon",
+    type: "image/png",
+    href: "/public/sponsors/favicon.ico",
+  }
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
